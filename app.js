5a681343371610386c9b31c560088f3d564d2c6e
belal <belal20795@gmail.com>
2019-09-11 22:51:35 +0530

    removed unncessary comments

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-
-// function submit = 
-// document.getElementById("submit").addEventListener("onclick", function(event)  {
-//     event.preventDefault();
-//     console.log("done");    
-//     alert("hello");
-// }, true);
-
-// submit.addEventListener("onclick", function submit(e) {
-//     e.preventDefault();
-//     const textarea = document.getElementById("messageField");
-//     let textcontent = textarea.innerHTML;
-//     console.log(textcontent);
-//     alert("helo")
-//   });
-
-
-
-// function show(evt) {
-//     evt.preventDefault();
-    
-//     alert("hello");
-// }
-
-
 const form = document.querySelector("form");
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const NameValue = document.getElementById("formName").value;
     const textValue = document.getElementById("messageField").value;
-
+    
     console.log(NameValue);
     console.log(textValue);
 });
