742ee6a9d8b3ad1584de00a063eb132b94d646ca
belal <belal20795@gmail.com>
2019-09-11 23:01:47 +0530

    changes were done

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+
+// function submit = 
+// document.getElementById("submit").addEventListener("onclick", function(event)  {
+//     event.preventDefault();
+//     console.log("done");    
+//     alert("hello");
+// }, true);
+//     const submit = document.getElementById("formSubmit")
+//     submit.addEventListener("submit", function submit(e) {
+//     e.preventDefault();
+//     const textarea = document.getElementById("messageField");
+//     let textcontent = textarea.value;
+//     console.log(textcontent);
+//   });
+
+
+
+// function show(evt) {
+//     evt.preventDefault();
+    
+//     alert("hello");
+// }
+
+
 const form = document.querySelector("form");
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const NameValue = document.getElementById("formName").value;
     const textValue = document.getElementById("messageField").value;
-    
+
     console.log(NameValue);
     console.log(textValue);
 });
+
