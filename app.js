cdca3ec5e99a66dd5f121e71b81aa916cf560f6a
belal <belal20795@gmail.com>
2019-09-12 19:24:21 +0530

    single element added with p tag

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
 
-// function submit = 
-// document.getElementById("submit").addEventListener("onclick", function(event)  {
-//     event.preventDefault();
-//     console.log("done");    
-//     alert("hello");
-// }, true);
-//     const submit = document.getElementById("formSubmit")
-//     submit.addEventListener("submit", function submit(e) {
-//     e.preventDefault();
-//     const textarea = document.getElementById("messageField");
-//     let textcontent = textarea.value;
-//     console.log(textcontent);
-//   });
-
-
-
-// function show(evt) {
-//     evt.preventDefault();
-    
-//     alert("hello");
-// }
-
-
 const form = document.querySelector("form");
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    const NameValue = document.getElementById("formName").value;
+    var NameValue = document.getElementById("formName").value;
     const textValue = document.getElementById("messageField").value;
 
+
+    var para = document.createElement("p"); //creating a p element
+    var node = document.createTextNode(NameValue); //creating the value
+    // var pode = document.createTextNode(textValue);
+    para.appendChild(node);     // appending the child element <p> to the inside of it p element
+    // para.appendChild(pode);
+
+    var parent = document.getElementById("element"); //getting place where to put the element 
+    parent.appendChild(para);       // putting the element on the html using appendchild
+    
     console.log(NameValue);
     console.log(textValue);
 });
-
